306e8194629ca798b0bf04c3d13325d9737d8a44
khushboo nijhawan <79616524+KhushbooNijhawan@users.noreply.github.com>
2022-01-16 02:00:29 +0530

    Add files via upload

---
+++ src/components/MySkillsPage.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import {lightTheme} from './Themes';
+import { Design, Develope} from './AllSvgs';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+  
+align-items: center;
+// make Box fit to screen
+overflow: hidden;
+
+
+
+
+`
+
+const Main = styled.div`
+border: 2px solid ${props => props.theme.text};
+color: ${props => props.theme.text};
+background-color: ${props => props.theme.body};
+padding: 2rem;
+width: 30vw;
+height: 60vh;
+z-index:3;
+line-height: 1.5;
+cursor: pointer;
+
+font-family: 'Ubuntu Mono',monospace;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
+&:hover{
+    color: ${props => props.theme.body};
+    background-color: ${props => props.theme.text};
+}
+`
+
+const Title = styled.h2`
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: calc(1em + 1vw);
+
+${Main}:hover &{
+    &>*{
+        fill:${props => props.theme.body};
+    }
+}
+
+&>*:first-child{
+margin-right: 1rem;
+}
+`
+
+const Description = styled.div`
+color: ${props => props.theme.text};
+font-size: calc(0.6em + 1vw);
+padding: 0.5rem 0;
+
+
+${Main}:hover &{
+   
+        color:${props => props.theme.body};
+    
+}
+
+strong{
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+}
+ul,p{
+    margin-left: 2rem;
+}
+`
+
+const MySkillsPage = () => {
+    return (
+        <ThemeProvider theme={lightTheme}>
+<Box>
+
+<LogoComponent theme='light'/>
+<SocialIcons theme='light'/>
+<PowerButton />
+<ParticleComponent theme='light' />
+            <Main>
+<Title>
+    <Design width={40} height={40} /> Non-Technical Skills
+</Title>
+<Description>
+I beleive in giving back to society through the skills i have gained.
+</Description>
+<Description>
+
+<ul>
+    <li>
+         Operations of college club and other social activities
+    </li>
+    <li>
+        Professional blogging
+    </li>
+    <li>
+        Speaker at sessions for junior students
+    </li>
+    <li>
+        Influential skills as a book disturbuter and a speaker
+    </li>
+</ul>
+</Description>
+
+
+            </Main>
+
+
+
+
+
+
+
+
+
+
+
+
+            <Main>
+<Title>
+    <Develope width={40} height={40} /> Technical Skills
+</Title>
+<Description>
+I value in creating, and having fun with what i make.
+</Description>
+<Description>
+<strong>Skills</strong>
+<p>
+Html, Css, Bootstrap, React,Machine Learning, Python, Git, NodeJs etc.
+</p>
+</Description>
+<Description>
+<strong>Tools</strong>
+<p>
+VScode, Github, Pycharm etc.
+</p>
+</Description>
+
+            </Main>
+
+            <BigTitle text="SKILLS" top="80%" right="30%" />
+
+        </Box>
+
+        </ThemeProvider>
+        
+    )
+}
+
+export default MySkillsPage
